cc1a377afd487a7dc88a62b8cdf05b3e698c5e14
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2024-10-10 14:12:29 -0300

    Code organization

--- src/main.js
+++ src/main.js
@@ -2,96 +2,34 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let cameraX = 4.61
+let cameraX = 4.60;
 
-camera.position.set(cameraX, 2.74, 8);
+camera.position.set(cameraX, 2.75, 8);
 
-const renderer = new THREE.WebGLRenderer({
-  alpha: true,
-  antialias: true,
-});
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// Mouse Camera Control
+new OrbitControls(camera, renderer.domElement);
 
-class Box extends THREE.Mesh {
-  constructor(
-    { width, height, depth, color = "#00ff00", velocity = { x: 0, y: 0, z: 0 }, position = { x: 0, y: 0, z: 0 }, zAcceleration = false}
-  ) {
-    super(new THREE.BoxGeometry(width, height, depth), new THREE.MeshStandardMaterial({ color }));
+// Light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.y = 3;
+light.position.z = 1;
+light.castShadow = true;
 
-    this.width = width;
-    this.height = height;
-    this.depth = depth;
+scene.add(light);
 
-    this.position.set(position.x, position.y, position.z);
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
-    this.right = this.position.x + this.width / 2;
-    this.left = this.position.x - this.width / 2;
+camera.position.z = 5;
 
-    this.bottom = this.position.y - this.height / 2;
-    this.top = this.position.y + this.height / 2;
-
-    this.front = this.position.z + this.depth / 2;
-    this.back = this.position.z - this.depth / 2;
-
-    this.velocity = velocity;
-    this.gravity = -0.002;
-
-    this.zAcceleration = zAcceleration;
-    this.canJump = true;
-  }
-
-  updateSides() {
-    this.right = this.position.x + this.width / 2;
-    this.left = this.position.x - this.width / 2;
-
-    this.bottom = this.position.y - this.height / 2;
-    this.top = this.position.y + this.height / 2;
-
-    this.front = this.position.z + this.depth / 2;
-    this.back = this.position.z - this.depth / 2;
-  }
-
-  update(ground) {
-    this.updateSides();
-
-    if (this.zAcceleration) this.velocity.z += 0.0003;
-
-    this.position.x += this.velocity.x;
-    this.position.z += this.velocity.z;
-
-    this.applyGravity(ground);
-  }
-
-  applyGravity(ground) {
-    this.velocity.y += this.gravity;
-
-    if (boxCollision({ box1: this, box2: ground })) {
-      const friction = 0.5;
-      
-      this.velocity.y *= friction;
-      this.velocity.y = -this.velocity.y;
-
-      this.canJump = true;
-    } else {
-      this.position.y += this.velocity.y;
-
-      this.canJump = false;
-    }
-  }
-}
-
-// Cronômetro e elementos de interface
+// Timer and Interface Elements
 let timerInterval;
 let timer;
 let bestTime = 0;
@@ -186,13 +124,73 @@ function restartGame() {
   animate();
 }
 
-function boxCollision({ box1, box2 }) {
-  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
-  const yCollision =
-    box1.bottom + box1.velocity.y <= box2.top && box1.top >= box2.bottom;
-  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
+// Mesh Elements
+class Box extends THREE.Mesh {
+  constructor(
+    { width, height, depth, color = "#00ff00", velocity = { x: 0, y: 0, z: 0 }, position = { x: 0, y: 0, z: 0 }, zAcceleration = false}
+  ) {
+    super(new THREE.BoxGeometry(width, height, depth), new THREE.MeshStandardMaterial({ color }));
 
-  return xCollision && yCollision && zCollision;
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+
+    this.position.set(position.x, position.y, position.z);
+
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
+
+    this.velocity = velocity;
+    this.gravity = -0.002;
+
+    this.zAcceleration = zAcceleration;
+    this.canJump = true;
+  }
+
+  updateSides() {
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
+  }
+
+  update(ground) {
+    this.updateSides();
+
+    if (this.zAcceleration) this.velocity.z += 0.0003;
+
+    this.position.x += this.velocity.x;
+    this.position.z += this.velocity.z;
+
+    this.applyGravity(ground);
+  }
+
+  applyGravity(ground) {
+    this.velocity.y += this.gravity;
+
+    if (boxCollision({ box1: this, box2: ground })) {
+      const friction = 0.5;
+      
+      this.velocity.y *= friction;
+      this.velocity.y = -this.velocity.y;
+
+      this.canJump = true;
+    } else {
+      this.position.y += this.velocity.y;
+
+      this.canJump = false;
+    }
+  }
 }
 
 const cube = new Box({
@@ -201,6 +199,7 @@ const cube = new Box({
   depth: 1,
   velocity: { x: 0, y: -0.01, z: 0 },
 });
+
 cube.castShadow = true;
 scene.add(cube);
 
@@ -209,26 +208,13 @@ const ground = new Box({
   height: 0.5,
   depth: 40,
   color: "#0c4b6e",
-  position: {
-    x: 0,
-    y: -2,
-    z: 0,
-  },
+  position: { x: 0, y: -2, z: 0 },
 });
 
 ground.receiveShadow = true;
 scene.add(ground);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.y = 3;
-light.position.z = 1;
-light.castShadow = true;
-scene.add(light);
-
-scene.add(new THREE.AmbientLight(0xffffff, 0.5));
-
-camera.position.z = 5;
-
+// Controls
 const keys = {
   a: { pressed: false },
   d: { pressed: false },
@@ -245,20 +231,25 @@ window.addEventListener("keydown", (event) => {
     case "KeyA":
       keys.a.pressed = true;
       break;
+
     case "KeyD":
       keys.d.pressed = true;
       break;
+
     case "KeyS":
       keys.s.pressed = true;
       break;
+
     case "KeyW":
       keys.w.pressed = true;
       break;
+
     case "ShiftLeft":
       cameraX = -cameraX
-      camera.position.set(cameraX, 2.74, 8);
+      camera.position.set(cameraX, 2.75, 8);
       camera.lookAt(scene.position);
       break;
+
     case "Space":
       if (cube.canJump) {
         cube.velocity.y = 0.08;
@@ -273,17 +264,28 @@ window.addEventListener("keyup", (event) => {
     case "KeyA":
       keys.a.pressed = false;
       break;
+
     case "KeyD":
       keys.d.pressed = false;
       break;
+
     case "KeyS":
       keys.s.pressed = false;
       break;
+
     case "KeyW":
       keys.w.pressed = false;
       break;
   }
 });
+
+function boxCollision({ box1, box2 }) {
+  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
+  const yCollision = (box1.bottom + box1.velocity.y) <= box2.top && box1.top >= box2.bottom;
+  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
+
+  return xCollision && yCollision && zCollision;
+}
 
 const enemies = [];
 let frames = 0;
@@ -295,10 +297,11 @@ function animate() {
   const animationId = requestAnimationFrame(animate);
   renderer.render(scene, camera);
 
-  // Movimento
+  // Initial Position
   cube.velocity.x = 0;
   cube.velocity.z = 0;
 
+  // Movement
   if (keys.a.pressed) cube.velocity.x = -0.05;
   if (keys.d.pressed) cube.velocity.x = 0.05;
   if (keys.s.pressed) cube.velocity.z = 0.05;
@@ -326,6 +329,7 @@ function animate() {
     }
   });
 
+  // Enemy Spawning
   if (frames % spawnRate === 0) {
     if (spawnRate > 20) spawnRate -= 20;
 
@@ -338,14 +342,16 @@ function animate() {
       color: "#ff0000",
       zAcceleration: true,
     });
+
     enemy.castShadow = true;
+    
     scene.add(enemy);
+
     enemies.push(enemy);
   }
 
   frames++;
 }
 
-// Iniciar o cronômetro e a animação
 startTimer();
 animate();
